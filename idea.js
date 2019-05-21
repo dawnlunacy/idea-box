7309808ccc5edb707bcb19f7c68ea6a0e6dbb791
Alan Turing <alanturing@example.com>
2019-05-21 17:59:50 -0600

    Initialize psuedocode

---
+++ idea.js
@@ -0,0 +1,39 @@
+//create a class idea
+//have a constructor to create more instances of the class ie objects
+//set key of property to have a value of title
+//set key of property to have a value of body
+// set key property for star to have a default value of false
+// set key property for Quality to have a default value of: "Swill"
+
+//will need a function to save to storage saveToStorage (local storage)
+  //will need to obtain a value from the dom and be assigned to a key
+  //will need key:value pair for title
+  //will need key:value pair for body
+  // will need key:value pair for quality
+  //will need default value for property of star to be false- key value pair
+  //then take that key value pair and turn into a JSON object (stringify it)
+  // set JSON object to variable to prep for storage
+  //then use localstorage.setItem to set key value pair from JSON
+
+//updateIdea
+  //retrieve JSON from localStorage (localStorage.getItem) 
+  //parse JSON object to become its original JS data *key-value pair*
+  //access the array of properties for that JSON Object
+  //manipulate or edit the array but utilizing array prototype methods
+    //invoke save to storage function
+
+//deletefromStorage
+  //invoke update idea to manipulate data
+  //use localStorage.removeItem([key]) to update local storage
+
+//update quality
+ //we think first use updateIdea
+ //then use the splice array protoype method to chage the value of the key for the property of quality
+
+
+ 
+
+  
+
+
+
---
+++ main.js
@@ -0,0 +1,30 @@
+// assign a global variable to the html class of 'save-btn' called saveBtn
+// assign a global variable to the html class of 'new-quality-btn' called newQualityBtn
+// assign a global variable to the html class of 'starred-ideas-btn' called starredIdeasBtn
+
+// add event listener on click to saveBtn that invokes the ---function---
+
+
+
+
+
+
+
+//goal: function that calls the saveToStorage function and prints card onto DOM called createNewCard
+      //invoke saveToStorage function
+      //create new local variable with the html for new card (ensure star in included) called newCard
+          //create template literal for the user input of title
+          //create template literal for the user input of body
+      //create a variable (start global, keep local if possible) for the html class of "card-container"
+      //chain the javascript native method of insertAdjacentHTML with the parameters of ****'afterbegin'*** (test this with grid and flex properties), and newCard
+
+      //set up template literals
+          //initialize as global variable for title input (consider for refactoring)
+          //initialize as global variable for body input (consider for refactoring)
+          //pass these global variables through template literal notation with .value (look up correct terminology)
+          
+
+
+
+
+
