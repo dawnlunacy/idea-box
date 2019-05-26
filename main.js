58371b7997c33d87c7316342741f7eda37f3eaf9
Alan Turing <alanturing@example.com>
2019-05-26 15:26:07 -0600

    Add star via input fields, adjusted functions for generating idea

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ var starredIdeasBtn = document.querySelector('.starred-ideas-btn');
 var titleInput = document.querySelector('.title-input')
 var bodyInput = document.querySelector('.body-input')
 var cardDisplayArea = document.querySelector('#card-display-area')
-var starBtn = document.querySelectorAll('.stars')
+var starBtn = document.querySelector('.star-btn')
+var deleteBtn =document.querySelector('.delete-btn')
+var upvoteBtn =document.querySelector('.upvote-btn')
+var downvoteBtn =document.querySelector('.downvote-btn')
 var ideas = JSON.parse(localStorage.getItem('ideas')) || [];
  
 
@@ -24,7 +27,7 @@ function newQualityBtn(e){
 
 function loadCards() {
   ideas.forEach(function(idea){
-    appendCard(idea.title, idea.body);
+    appendCard(idea);
   })
 }
 
@@ -42,27 +45,65 @@ function loadCards() {
 //     return number;
 //   }
 // })
+// function toggleStar(e){
+  
+
+  // var starImg = false;
+  // e.target.src.match('Images/star.svg') ? e.target.src ='Images/star-active.svg' : 
+  // e.target.src ='Images/star.svg';
+  // e.target.src.match('Images/star.svg') ? star = false : star = true;
+// };
+
+// function 
+
+////**** Will need a function to toggle star 
+//e.target.setAttribute(atributeyouwanttochange (src), the things you want to change it to (filepath) )
+//with this function we will need an event listener to listen for when them starimg is clicked
+//upon click - toggle imgs  -- see line 60//
+    //still need to get this instance of this idea
+    //need to match the idea of the card you are on
+    //relying on event listener ( e.target.closest(".card-display").getAttribute(data-id))
+    //declare a variable and assign to above e.target event and that new variable will be the id of that card
+    //now go through entire array and find the one id that matches that new variable and pull it out 
+    //(look into array protype called find) (nameofArray.find(callbackfunction)function(idea){
+    //   return idea.data-id === varible you set 
+    // })
+
+  // make a function example Find Idea with a parameter.
+  //  and then invoke it in the previous function and pass through as an argument the variable you created earlier to define the id/data attribute of the card you are now targeting
+  //
+  
+
+  // inside the delcaration of the function write out callback function
+// make the call back function that returns ONLY the idea  that matches the data attribute and then assign that to a new variable. 
+//
 
 
-function appendCard(title, body) {
-  var newCard = new Idea(title, body)
+
+
+
+function appendCard({title, body, star, quality, qualitySelect, id}) {
+  // var newCard = new Idea(title, body)
+  var starImg = star ? "images/star-active.svg" : "images/star.svg"
+    // if(star === 'true'){
+    //   return "images/star-active.svg"
+    // } else {
+    //   return "images/star.svg"
+    // };
+    console.log(starImg)
   var cardToAppend = 
-  `  <article class="card-display"> 
+  `  <article class="card-display" data-id=${id}>
     <body>
     <header>
-      <img src='images/star.svg' class="stars star-default">
-      <img src='images/star-active.svg' class="stars star-active" onclick="">
-      <img src='images/delete.svg' class="delete-default">
-      <img src='images/delete-active.svg' class="delete-active">
+    <input type="image" src=${starImg} class="star-btn" alt="star-button">
+     <input type ='image' src='images/delete.svg' class='delete-btn' alt='delete-button'>
     </header>
-    <h3 class="card-title">${title}</h3>
-      <p class="card-body">${body}</p>
-     <footer class="card-footer">
-     <img src='images/upvote.svg' class="upvote-default">
-     <img src='images/upvote-active.svg' class="upvote-active">
-     <h5 class="card-footer">Quality: Swill</h5>
-     <img src='images/downvote.svg' class="downvote-default">
-     <img src='images/downvote-active.svg' class="downvote-active">
+    <h3 class='card-title' contenteditable='true'>${title}</h3>
+      <p class='card-body' contenteditable='true'>${body}</p>
+     <footer class='card-footer'>
+     <input type='image' src='images/upvote.svg' class='upvote-btn' alt='upvote-button'>
+     <h5 class='card-footer-text'>${qualitySelect[quality]}</h5>
+     <input type='image' src='images/downvote.svg' class='downvote-btn' alt='downvote-button'>
      </footer>
     </body>
   </article>`
@@ -71,10 +112,36 @@ function appendCard(title, body) {
 
 function saveBtnHelper(e){
   e.preventDefault();
-  var title = titleInput.value;
-  var body = bodyInput.value;
-  appendCard(title, body);
-  saveCard(title, body)
+  createIdea();
+  // var title = titleInput.value;
+  // var body = bodyInput.value;
+  // appendCard({title, body, star, quality, id});
+  // saveCard(title, body)
+}
+//upon hitting save button
+    //first instantiate an idea
+    //pass the arguments for the parameters of that idea. 
+    //pass that idea to your function to append card with that idea
+
+    
+    //page should not reload
+    //idea should be pushed into the array(should persist through page load)
+
+function createIdea(){
+  var idea = new Idea(titleInput.value, bodyInput.value, false, 0, Date.now())
+//instantiate class and pass through arguments for your parameters)
+ideas.push(idea)
+//push that newidea into your array 
+idea.saveToStorage(ideas)
+//store that array
+titleInput.value = " ";
+bodyInput.value = " ";
+appendCard(idea);
+console.log(idea)
+//then reset the title and body input to an empty string
+//call the function to display your idea on a card and pass through the argument of the variable that is this new instance
+// then disable the save 
+
 }
 
 function saveCard(title, body) {
